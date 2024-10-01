4bf50aaba6f5340b91167a2c274888ef7a4c9047
Rapahel <raphael.spuhler@students.fhnw.ch>
2024-10-01 12:10:38 +0000

    change struct.c to return a pointertype

--- struct.c
+++ struct.c
@@ -2,13 +2,17 @@
 
 typedef struct { int x; int y; } Point;
 
-Point create_point(int x, int y) {
-    Point p = {x, y};
+Point *create_point(int x, int y) {
+    // Point p = {x, y}; // Stack
+    // return &p;
+    Point *p = malloc(sizeof(Point)); // Heap
     return p;
+    p->x = y;
+    p->y = y;
 }
 
 int main() {
-    Point origin = create_point(0, 0);
-    printf("(%d, %d)\n", origin.x, origin.y);
+    Point *origin = create_point(0, 0);
+    printf("(%d, %d)\n", origin->x, origin->y);
     return 0;
 }
